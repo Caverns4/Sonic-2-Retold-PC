45d289dd6b8ff00a467a08ffa686bf48bbd23624
Caverns4 <icegod64@gmail.com>
2024-08-01 17:05:40 -0400

    EHZ Boss progress, CPZ scrolling, HTZ lava

--- Entities/Boss/Boss Components/DrillEggmanWheel.gd
+++ Entities/Boss/Boss Components/DrillEggmanWheel.gd
@@ -1,5 +1,32 @@
 extends CharacterBody2D
 
+@export_enum("Front","Back") var wheelType: String = "Front"
+var anim = "Front_RESET"
+
+func _ready():
+	if wheelType == "Back":
+		z_index = 0
+	else:
+		z_index = 2
+
 func _physics_process(delta):
-	if is_on_floor():
+	if !is_on_floor():
+		velocity.y += 9.8*delta
+	else:
 		velocity.y = 0
+	move_and_slide()
+	
+func updateAnim(xMove):
+	if xMove < 0:
+		$Sprite2D.flip_h = false
+	elif xMove > 0:
+		$Sprite2D.flip_h = true
+	
+	anim = wheelType
+	if xMove != 0:
+		anim += "_Drive"
+	else:
+		anim += "_RESET"
+	
+	$AnimationPlayer.play(anim)
+
--- Entities/Boss/Boss Components/DrillEggman_Car.gd
+++ Entities/Boss/Boss Components/DrillEggman_Car.gd
@@ -1,7 +1,8 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var range = 512 # Range in pixels from the center to drive
 var ignition = false # If the vehicle is started
+var direction = 1.0 #-1 or 1
 
 @onready var tires = [
 	#Back wheels
@@ -11,26 +12,56 @@ var ignition = false # If the vehicle is started
 	$DrillEggmanWheel3,
 	$DrillEggmanWheel4
 	]
-	
 @onready var drill = $DrillEggmanDrill
 
-func _input(event):
-	if Input.is_action_pressed("gm_left"):
-		scale.x = 1
-		position.x -=1
-	if Input.is_action_pressed("gm_right"):
-		scale.x = -1
-		position.x += 1
+
+func _ready():
+	tires[0].top_level = true
+	tires[0].global_position.y = global_position.y
+	tires[1].top_level = true
+	tires[1].global_position.y = global_position.y
+	tires[2].top_level = true
+	tires[2].global_position.y = global_position.y
+	tires[3].top_level = true
+	tires[3].global_position.y = global_position.y
 
 func _process(delta):
-	tires[0].position.x = position.x - (-32 * scale.x)
-	tires[1].position.x = position.x - (-9 * scale.x)
-	tires[2].position.x = position.x - (15 * scale.x)
-	tires[3].position.x = position.x - (36 * scale.x)
+	velocity.x = 0
 
 func _physics_process(delta):
+	if Input.is_action_just_pressed("gm_left_P2"):
+		direction = 1.0
+		$Sprite2D.flip_h = false
+	if Input.is_action_just_pressed("gm_right_P2"):
+		direction = -1.0
+		$Sprite2D.flip_h = true
+	if Input.is_action_pressed("gm_left_P2"):
+		velocity.x = 0-direction*50
+	if Input.is_action_pressed("gm_right_P2"):
+		velocity.x = 0-direction*50
+	SetTirePositions()
+
 	var posbuff = (tires[0].global_position.y + tires[1].global_position.y)/2
 	posbuff += (tires[2].global_position.y + tires[3].global_position.y)/2
 	posbuff = round(posbuff/2) - 16
 	global_position.y = posbuff
-	pass
+	
+	move_and_slide()
+	UpdateDrillIfAttached()
+
+func SetTirePositions():
+	tires[0].position.x = position.x + (-38 * direction)
+	tires[1].position.x = position.x + (-20 * direction)
+	tires[2].position.x = position.x + (15 * direction)
+	tires[3].position.x = position.x + (36 * direction)
+	
+	for i in tires.size():
+		tires[i].updateAnim(velocity.x)
+
+func UpdateDrillIfAttached():
+	drill.global_position.x = global_position.x + (-54 * direction)
+	drill.global_position.y = global_position.y + 8
+	if direction > 0:
+		drill.get_child(0).flip_h = false #Sprite2D
+	else:
+		drill.get_child(0).flip_h = true #Sprite2D
