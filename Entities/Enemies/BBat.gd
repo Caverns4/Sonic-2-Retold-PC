cdcd119c06790dc20d3b3f32e95505c64ac5b8bb
Caverns4 <icegod64@gmail.com>
2024-08-06 20:19:58 -0400

    Hidden Palace Progress

--- Entities/Enemies/BBat.gd
+++ Entities/Enemies/BBat.gd
@@ -1,6 +1,17 @@
 extends EnemyBase
-
 # Todo
 
+var hoverOffset = 0.0
+
+func _process(delta):
+	super(delta)
+
 func _physics_process(delta):
+	updateHoveringPos(delta)
 	pass
+
+func updateHoveringPos(delta):
+	# change the hover offset
+	global_position.y = global_position.y-hoverOffset
+	hoverOffset = move_toward(hoverOffset,cos(Global.levelTime*4)*4,delta*10)
+	global_position.y = global_position.y+hoverOffset
