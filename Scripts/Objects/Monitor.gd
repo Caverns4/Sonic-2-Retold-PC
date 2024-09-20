dc13dd269fe95c3fc7272c387b016bb3c6ffc428
Caverns4 <icegod64@gmail.com>
2024-09-20 03:53:27 -0400

    New Shield system

--- Scripts/Objects/Monitor.gd
+++ Scripts/Objects/Monitor.gd
@@ -60,13 +60,13 @@ func destroy():
 		if item == ITEMTYPES.LIFEP2:
 			$Item.frame = item + Global.PlayerChar2
 	
-	if item == ITEMTYPES.SHIELD and !Global.TwoPlayer:
-		var rand = randi_range(0,32)
-		if rand == 3:
+	if item == ITEMTYPES.SHIELD and !Global.TwoPlayer and playerTouch.shield > 0:
+		var rand = randi_range(0,2)
+		if rand == 0:
 			item = ITEMTYPES.FIRESHIELD
-		elif rand == 12:
+		elif rand == 1:
 			item = ITEMTYPES.ELECSHIELD
-		elif rand == 27:
+		elif rand == 2:
 			item = ITEMTYPES.BUBBLESHIELD
 		$Item.frame = item + 2
 	
