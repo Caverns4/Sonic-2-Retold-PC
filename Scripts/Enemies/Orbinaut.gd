7601055cc88c565e2d52bd6da9e1d41fdaf31d32
Caverns4 <icegod64@gmail.com>
2024-10-02 19:06:55 -0400

    Sol can launch orbs, Casino Night Music import

--- Scripts/Enemies/Orbinaut.gd
+++ Scripts/Enemies/Orbinaut.gd
@@ -10,6 +10,11 @@ var spinOffset = 0
 @export var launchOrbs = false
 
 @onready var orbList = [get_node("Orb")]
+var orbStates = [false] #State of each orb in the array, if flung
+var orblifeSpan = [0] #Time orb has been alive after firing.
+
+var targets = []
+var triggered = false
 
 func _ready():
 	if !Engine.is_editor_hint():
@@ -24,8 +29,11 @@ func _ready():
 			var newOrb = $Orb.duplicate()
 			add_child(newOrb)
 			orbList.append(newOrb)
+			orbStates.insert(orbStates.size(),false)
+			orblifeSpan.insert(orblifeSpan.size(),0)
 		
 		$VisibleOnScreenEnabler2D.visible = true
+		$PlayerCheck.visible = true
 
 func _process(delta):
 	if Engine.is_editor_hint():
@@ -39,6 +47,27 @@ func _physics_process(delta):
 		spinOffset += speed*delta
 		# rotate the orbs based on spinOffset
 		for i in range(orbList.size()):
-			var getOrb = orbList[i]
-			getOrb.position = (Vector2.RIGHT*distance).rotated(deg_to_rad(spinOffset+((360.0/orbs)*i)))
-		# Launch base behaviour
+			if orbList[i]:
+				var getOrb = orbList[i]
+				if !orbStates[i]:
+					getOrb.position = (Vector2.RIGHT*distance).rotated(deg_to_rad(spinOffset+((360.0/orbs)*i)))
+				else:
+					#getOrb.position.x += 0-(sign(speed) * (delta*60))
+					getOrb.position.x -= sign(speed) * (delta*60)
+					orblifeSpan[i]+=delta
+			
+				if orblifeSpan[i] > 6.0:
+					getOrb.queue_free()
+					orbList[i] = null
+			
+				if launchOrbs and triggered and !orbStates[i]:
+					if round(getOrb.position.y * sign(speed)) == distance:
+						orbStates[i] = true
+
+func _on_player_check_body_entered(body: Node2D) -> void:
+	targets.append(body)
+	triggered = true
+
+
+func _on_player_check_body_exited(body: Node2D) -> void:
+	targets.erase(body)
