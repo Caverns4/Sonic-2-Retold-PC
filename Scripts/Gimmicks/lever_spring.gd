7898387acbf353e87f4b047478a998e35ae8a1f8
Caverns4 <icegod64@gmail.com>
2024-10-18 03:49:22 -0400

    Pressure-sensitive spring testing

--- Scripts/Gimmicks/lever_spring.gd
+++ Scripts/Gimmicks/lever_spring.gd
@@ -1,19 +1,58 @@
 extends StaticBody2D
 
+const DOWN_TIME = 1.0/30.0
 
 var players = []
-var TotalWeight = 0.0
+var direction = 1
+var WeightVal = 0.0
+var downTimer = 0.0
+
+var lauchSpeeds = [0, 1,  1,  2,  2,  3,  3,  3,  4, 4, 4, 4]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	direction = clampf(global_scale.x,-1,1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	downTimer -= delta
+	
+	if downTimer <= 0.0 and $Sprite2D.frame != 0:
+		$Sprite2D.frame = 0
+		$SpringSound.play()
+		SpringCharacters()
+		players = []
+	
 	if $Sprite2D.frame != 0:
 		$CollisionPolygon2D.scale.y = 0.4
 	else:
 		$CollisionPolygon2D.scale.y = 1.0
-	
-	
+
+func physics_collision(body, hitVector):
+	#If player(s) are standing on the objects, detect the furthest one out.
+	if hitVector.y > 0.0:
+		players.append(body)
+		if downTimer <= 0.0:
+			var temp = max(abs(body.global_position.x - global_position.x),0)
+			temp -= (8*direction)
+			if temp > 0:
+				downTimer = DOWN_TIME
+				$Sprite2D.frame = 1
+
+func SpringCharacters():
+	for i in players.size():
+		var node = players[i]
+		var temp = (global_position.x - 20 - node.global_position.x)/8
+		#print(temp)
+		var speedSet = lauchSpeeds[max(abs(temp),0)] * 60
+
+		node.set_state(node.STATES.AIR)
+		node.ground = false
+		node.airControl = true
+		#node.animator.play("spring")
+		node.animator.play("springScrew")
+		node.animator.queue("walk")
+		node.movement.y = -240 - (speedSet)
+		if abs(node.movement.x) > 400:
+			speedSet *= direction
+			node.movement.x -= speedSet
