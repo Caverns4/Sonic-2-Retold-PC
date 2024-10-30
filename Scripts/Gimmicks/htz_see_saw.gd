60760818779076df058cad51fb9216cdbb87ab90
Caverns4 <icegod64@gmail.com>
2024-10-29 20:23:48 -0400

    HPZ collision further fixes, HTZ fixes

--- Scripts/Gimmicks/htz_see_saw.gd
+++ Scripts/Gimmicks/htz_see_saw.gd
@@ -171,6 +171,7 @@ func clampChildXCoords():
 
 func physics_collision(body, hitVector):
 	if hitVector.y > 0 and body.ground:
+		body.angle = 0
 		if !weights.has(body):
 			weights.append(body)
 			#print("PLAYER LAND")
