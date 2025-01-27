b2fb7b7c7ff8d33b3de4b079ed16bb02a6930b9d
Caverns4 <Caverns4@users.noreply.github.com>
2025-01-27 14:09:38 -0500

    Auqis Add, Act 2 start

---
+++ Scripts/Enemies/aquis.gd
@@ -0,0 +1,20 @@
+extends EnemyBase
+
+
+const SPEED = 30.0
+
+
+func _ready() -> void:
+	$VisibleOnScreenEnabler2D.visible = true
+
+func _physics_process(delta: float) -> void:
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
