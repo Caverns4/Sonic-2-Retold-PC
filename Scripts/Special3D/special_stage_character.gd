32397f1146809cb601d841753ce937e0b6192042
Caverns4 <Caverns4@users.noreply.github.com>
2025-02-24 17:44:27 -0500

    Wall running test

--- Scripts/Special3D/special_stage_character.gd
+++ Scripts/Special3D/special_stage_character.gd
@@ -2,41 +2,77 @@ extends CharacterBody3D
 
 @onready var sfx = $SFX.get_children()
 
-const SPEED = 6.0
+const TOP_SPEED = 6.0
 const JUMP_VELOCITY = 6.5
+
+#Collision management
+var floor_ray = RayCast3D.new()
+var ray_length = 2.0  # Length for surface detection
 
 #Current Ring count
 var rings: int = 0
 var ringChannel = 0
 
 # Movement
-var inertia = 0
-var angle = 0
+var xform : Transform3D
+# Input movement ignoring rotations.
+var inertia : Vector2 = Vector2.ZERO # Lateral motion
+var localMovement = Vector3.ZERO #The above, plus jumping movement.
+# Base movement. This is likely going to be 0 most of the time.
+var movement = Vector3.ZERO
 
 func _ready() -> void:
 	Global.players.append(self)
+	self.add_child(floor_ray)
+	floor_ray.enabled = true
+	floor_ray.target_position = Vector3.DOWN * ray_length
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += up_direction * -9.8 * delta
+	#if !is_on_floor():
+	#	localMovement.y += 9.8 * delta
+	var direction = Vector3.ZERO
+	direction = handle_input(delta)
+	move_and_slide()
+	velocity -= direction * TOP_SPEED
+	
+	rotate_to_floor_angle()
 
+func handle_input(delta):
 	# Handle jump.
 	if isActionPressed() and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		localMovement = up_direction * (0-JUMP_VELOCITY)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Only the left/right inputs are given to the player.
+	# Forward and backward are controlled by the game.
 	var input_axis := Input.get_axis("gm_left","gm_right")
-	var direction := (transform.basis * Vector3(input_axis, 0, 0)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = -SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = -SPEED
+	
+	var direction = (global_basis * Vector3(input_axis,0,0)).normalized()
+	direction += (global_basis * Vector3(0,0,-1))
+	velocity += direction * TOP_SPEED
+	
+	return direction
 
-	move_and_slide()
+func rotate_to_floor_angle():
+	var surface_normal = get_floor()
+	if surface_normal:
+		#Align to nearest floor
+		xform = global_transform
+		xform.basis.y = surface_normal
+		xform.basis.x = -xform.basis.z.cross(surface_normal)
+		xform.basis = xform.basis.orthonormalized()
+		global_transform = xform
+		
+		up_direction = surface_normal.normalized()
+
+
+func get_floor():
+	# Check collision with the rays for floor, wall, and ceiling
+	if floor_ray.is_colliding():
+		#print(floor_ray.get_collision_normal())
+		return floor_ray.get_collision_normal()
+	return Vector3.ZERO
 
 func isActionPressed():
 	if Input.is_action_just_pressed("gm_action"):
