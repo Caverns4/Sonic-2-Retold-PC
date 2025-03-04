ee41c41abe06dc6d05479775df6c8eac3a341aaa
Caverns4 <Caverns4@users.noreply.github.com>
2025-03-04 02:55:45 -0500

    MCZ Hang Vines/Drawbridge

--- Scripts/Gimmicks/draw_bridge.gd
+++ Scripts/Gimmicks/draw_bridge.gd
@@ -2,13 +2,16 @@
 extends Node2D
 
 ## The direction the bridge will rotate when acivated.
-@export_enum("Counter-Clockwise","Clockwise") var RotateDirection = 0
+@export_enum("Counter-Clockwise","Clockwise") var rotateDirection = 0
 
 var trigger = false
+var intialRotation = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	#Become either 1 or -1
+	rotateDirection = rotateDirection*2-1
+	intialRotation = rotation_degrees
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,7 +20,12 @@ func _process(delta: float) -> void:
 		var next = child.find_child("Sprite2D",false)
 		next.global_position = child.global_position
 		
-	if Engine.is_editor_hint():
-		pass
-	else:
-		pass
+	if !Engine.is_editor_hint() and trigger:
+		rotation_degrees = move_toward(
+			rotation_degrees,
+			intialRotation+(90*rotateDirection),
+			delta*64)
+
+func Trigger():
+	trigger = true
+	pass
---
+++ Scripts/Gimmicks/hanging_vine.gd
@@ -0,0 +1,39 @@
+@tool
+extends Node2D
+
+## The initial chain length.
+@export var length: int = 32
+## The distance in pixels for the hanger to travel when the players grab it.
+@export var distance: int = 8
+## The Node to be afftected when the player grabs the switch.
+@export var targetNode: Node2D = null
+
+var currentLength: float = 0
+
+func _ready() -> void:
+	currentLength = length
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if !Engine.is_editor_hint():
+		var touching = 0
+		for i in $Hanger.players:
+			var node = i[0]
+			if i[1]:
+				touching += 1
+				timer = 0.1
+		if touching > 0:
+			currentLength = move_toward(currentLength,distance+abs(length),delta*128)
+			if targetNode and targetNode.has_method("Trigger") and !targetNode.trigger:
+				targetNode.Trigger()
+		else:
+			currentLength = move_toward(currentLength,abs(length),delta*128)
+	else:
+		currentLength = length
+	updateChainLength()
+
+
+func updateChainLength():
+	$TextureRect.size.y = abs(currentLength)
+	$Sprite2D.position.y = abs(currentLength)
+	$Hanger.position.y = $Sprite2D.position.y + 24
