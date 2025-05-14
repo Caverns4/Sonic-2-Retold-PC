df7ed09065c4ab9665771dfa5ae633604750c5c9
Caverns4 <Caverns4@users.noreply.github.com>
2025-05-13 23:00:54 -0400

    CNZ bumper behavior improvement

--- Scripts/Gimmicks/cnz_big_bumper.gd
+++ Scripts/Gimmicks/cnz_big_bumper.gd
@@ -1,21 +1,22 @@
 extends StaticBody2D
 
+@export var power = 480
 var bounceSound = preload("res://Audio/SFX/Objects/CNZ_BigBumper.wav")
 
 #When a body(player) enters the area and it not controlled by an outside object,
 #Bounce off based on the angle of the player relative to the object.
 
 func physics_collision(body, hitVector):
-	#Placeholder until I write a better method
-	#body.movement = (body.global_position-global_position).normalized()*7*60.0
-	hitVector = hitVector.rotated(global_rotation)
-	
-	print(hitVector)
-	body.movement = (hitVector.rotated(rad_to_deg(180.0)).normalized()*120.0
-	+ (Vector2(0,-480).rotated(global_rotation)))
+	var col = body.objectCheck.get_collision_normal()
+	#print(col)
+	if col:
+		body.movement = (hitVector.rotated(rad_to_deg(180.0)).normalized()*(power/4)
+		+ (Vector2(0,-power).rotated(global_rotation)))
+	else:
+		body.movement = Vector2.UP.rotated(global_rotation)*power
 	
 	body.set_state(body.STATES.AIR)
+	body.ground = false
 	#if body.currentState == body.STATES.JUMP: # set the state to air
 	#	body.set_state(body.STATES.AIR)
-	
 	Global.play_sound(bounceSound)
--- Scripts/Gimmicks/cnz_triangle_bumper.gd
+++ Scripts/Gimmicks/cnz_triangle_bumper.gd
@@ -1,19 +1,22 @@
 extends StaticBody2D
 
+@export var power = 480
 var bounceSound = preload("res://Audio/SFX/Objects/CNZ_BigBumper.wav")
 
 #When a body(player) enters the area and it not controlled by an outside object,
 #Bounce off based on the angle of the player relative to the object.
 
 func physics_collision(body, hitVector):
-	#Placeholder until I write a better method
-	#body.movement = (body.global_position-global_position).normalized()*7*60.0
-	
-	#print(hitVector)
 	var col = body.objectCheck.get_collision_normal()
-	print(col)
+	#print(col)
 	
-	body.movement = (hitVector.rotated(rad_to_deg(180.0)).normalized()*600.0)
+	if col:
+		body.movement = (col.normalized()*power)
+	else:
+		if abs(body.global_position.x - global_position.x) < 30:
+			body.movement.y = power*sign(body.global_position.y - global_position.y)
+		else:
+			body.movement.x = -power*sign(global_scale.x)
 	
 	if body.currentState == body.STATES.JUMP: # set the state to air
 		body.set_state(body.STATES.AIR)
