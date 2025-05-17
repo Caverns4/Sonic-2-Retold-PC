f5be2016cc05b205133d5607c30965712c0c741f
Caverns4 <Caverns4@users.noreply.github.com>
2025-05-16 21:36:51 -0400

    Metropolis layout work, more setup for rotating mesh

--- Scripts/Gimmicks/corkscrew_h_tube.gd
+++ Scripts/Gimmicks/corkscrew_h_tube.gd
@@ -1,29 +1,34 @@
 extends Node2D
 
+# Todo: Behavior
+#if a player enters the area range, wait for the player to "land" at the
+#bottom before starting to spin the player up and own in their corkscrew
+#animation.
 
-var players = []
+# length of the corkscrew
+@export var length = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+# player tracking arrays
+var playerList = []
+var playerTimers = []
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	
 	pass
-
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if Global.players.has(body):
-		players.append(body)
+		playerList.append(body)
 		var node = Global.players[Global.players.find(body)]
-		node.translate = true
+		if body.ground:
+			node.allowTranslate = true
+			node.set_state(body.STATES.CORKSCREW)
 		node.controlObject = self
-		node.set_state(body.STATES.CORKSCREW)
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if Global.players.has(body) and body.controlObject == self:
 		var node = Global.players[Global.players.find(body)]
-		node.translate = false
-	players.erase(body)
+		node.allowTranslate = false
+	playerList.erase(body)
