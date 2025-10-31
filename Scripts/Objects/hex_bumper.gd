f8d42c38e78a3c75aa46635d32cca5fc5e65bfea
Caverns4 <Caverns4@users.noreply.github.com>
2025-10-31 01:23:10 -0400

    Casino Night ring & Object placement

--- Scripts/Objects/hex_bumper.gd
+++ Scripts/Objects/hex_bumper.gd
@@ -1,6 +1,9 @@
 extends StaticBody2D
 
 @export var power: float = 360
+@export var move_distance: Vector2 = Vector2.ZERO
+
+var home: Vector2 = global_position
 
 func physics_collision(body, _hitVector):
 	var col = body.objectCheck.get_collision_normal()
